ebba0e4b458ad8cd7c22e3fb56f5f16510ecf114
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2020-02-11 21:55:59 +0300

    add swagger to project

--- Accoon.EventSource.PitStop/src/VehicleManagement/PitStop.VehicleManagement.Api/Startup.cs
+++ Accoon.EventSource.PitStop/src/VehicleManagement/PitStop.VehicleManagement.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using PitStop.VehicleManagement.Api.Common;
 using PitStop.VehicleManagement.Application;
 using PitStop.VehicleManagement.Application.Interfaces;
@@ -42,10 +43,23 @@ namespace PitStop.VehicleManagement.Api
             {
                 options.SuppressModelStateInvalidFilter = true;
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contacts API V1");
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
